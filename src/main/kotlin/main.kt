3f5a775742e410a49f29561548fce3f8fa229cf2
Masato Nagashima <longisland.1016@gmail.com>
2019-12-10 15:32:40 +0900

    IntelliJからビルドするとkotlin.jsがどこにもないが、terminalからだとkotlin.jsがある。よく分からないがとりあえず動く

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,8 @@
+import js.externals.jquery.jQuery
 import kotlin.browser.document
+import kotlin.browser.window
 
 fun main() {
     document.write("Hello, world!")
+    jQuery("footer").hover { window.alert("Hello!") }
 }
